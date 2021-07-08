e1886100eaf14950f12618e13b9a8145d57f6538
mishania6678 <mishania6678@gmail.com>
2021-07-08 20:21:41 +0300

    test

--- task4.py
+++ task4.py
@@ -1,10 +1,10 @@
 def power(x, y):
     # return x ** y
     static_x = x
-    for i in range(y - 1):
-        x *= static_x
+    for i in range(abs(y)):
+        x *= 1 / static_x
     return x if x != 0 and y != 0 else '0^0 неопределено'
 
 
-num1, num2 = float(input()), int(input())
+num1, num2 = int(input()), int(input())
 print(power(num1, num2))
